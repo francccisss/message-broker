a1eebdcdab8197dce87ab1e1ef0fd33e42e4abb4
francis <franzdiaz460@gmail.com>
2025-01-15 12:30:49 +0400

    test: adding test for client to server connections

---
+++ internal/server/server_test.go
@@ -0,0 +1,68 @@
+package server_test
+
+import (
+	"log"
+	"message-broker/internal/server"
+	"net"
+	"sync"
+	"testing"
+	"time"
+)
+
+const clientCount = 10
+
+func TestServerConnections(t *testing.T) {
+
+	s := server.NewServer("", "8080")
+	serverState := make(chan bool)
+	ln, err := s.ServeTCP()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	go func(s net.Listener, state chan bool) {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				state <- false
+			}
+			log.Printf("Connected %s", conn.RemoteAddr())
+		}
+	}(ln, serverState)
+
+	log.Println("Waiting for server")
+	dr := time.Duration(3) * time.Millisecond
+	time.Sleep(dr)
+	log.Println("Start Connecting")
+	go createClients(clientCount)
+	select {
+	case state := <-serverState:
+		if state == false {
+			t.Fatalf("Unable to listen to client connections")
+		}
+	}
+}
+
+func createClients(num int) {
+	var wg sync.WaitGroup
+	for i := range num {
+		wg.Add(i)
+		go func(i int) {
+			defer wg.Done()
+
+			conn, err := net.Dial("tcp", "localhost:8080")
+			log.Printf("Client connected %d", i)
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+
+			defer conn.Close()
+			t := time.Duration(i) * time.Millisecond
+			log.Printf("Time to sleep %d", t)
+			time.Sleep(t)
+			log.Printf("Wake up thread")
+		}(i)
+	}
+	wg.Wait()
+	log.Printf("Done")
+}
